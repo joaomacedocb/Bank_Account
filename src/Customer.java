1b97b77f4dd5c98838d8110fca9a96ab6840275d
joaomacedocb <joaomacedocb@gmail.com>
2024-02-17 23:31:42 -0300

    Preparing getters and setters

--- src/Customer.java
+++ src/Customer.java
@@ -14,18 +14,32 @@ public class Customer {
     String getDocument() {
         return this.document;
     }
-
     String getName() {
         return this.name;
     }
-
     int getAge() {
         return this.age;
     }
-
     int getCustomerAccount() {
         return this.customerAccount;
     }
-
+    boolean getIsActive(){
+        return this.isActive;
+    }
+    void setName(String name) {
+        this.name = name;
+    }
+    void setAge(int age) {
+        this.age = age;
+    }
+    void setDocument(String document){
+        this.document = document;
+    }
+    void setCustomerAccount(int customerAccount) {
+        this.customerAccount = customerAccount;
+    }
+    void setIsActive (boolean isActive) {
+        this.isActive = isActive;
+    }
 
 }
