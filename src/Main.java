68162e88f13630c7f7fa28e4ab945f3859027328
joaomacedocb <joaomacedocb@gmail.com>
2024-02-18 23:00:51 -0300

    Signup process update

--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,23 @@ public class Main {
         boolean customerAnswer = sc.nextBoolean();
 
         if (customerAnswer) {
+            Scanner a = new Scanner(System.in);
+
             Customer customer1 = new Customer("00000000000","Default",1, false);
             Account account1 = new Account(9999,9999,0.00, customer1);
+
+            System.out.println("Que ótimo! Informe o seu nome:");
+            customer1.setName(a.nextLine());
+
+            System.out.println("Boas-vindas " + customer1.getName() + ". Vamos seguir o seu cadastro.");
+
+            System.out.println("Informe a sua idade:");
+            customer1.setAge(a.nextInt());
+
+            System.out.println("Certo! Agora digite o seu CPF. Obs.: Use apenas números.");
+            customer1.setDocument(a.nextLine());
+
+
         }else {
             System.out.println("Que pena! Te vejo numa próxima.");
         }
