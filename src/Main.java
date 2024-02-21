566a3edfc529b66fc766d23cf938c961773edc77
joaomacedocb <joaomacedocb@gmail.com>
2024-02-20 23:39:11 -0300

    Signup process update

--- src/Main.java
+++ src/Main.java
@@ -7,25 +7,26 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Deseja cadastrar uma nova conta?");
-        System.out.println("true para Sim | false para Não");
-        boolean customerAnswer = sc.nextBoolean();
+        System.out.println("1 - para 'Sim' ou 2 para 'Não'");
+        String customerAnswer = sc.nextLine();
 
-        if (customerAnswer) {
-            Scanner a = new Scanner(System.in);
+        if (customerAnswer.matches("^(?i)sim$")) {
 
             Customer customer1 = new Customer("00000000000","Default",1, false);
             Account account1 = new Account(9999,9999,0.00, customer1);
 
             System.out.println("Que ótimo! Informe o seu nome:");
-            customer1.setName(a.nextLine());
+            customer1.setName(sc.nextLine());
 
             System.out.println("Boas-vindas " + customer1.getName() + ". Vamos seguir o seu cadastro.");
 
             System.out.println("Informe a sua idade:");
-            customer1.setAge(a.nextInt());
+            int customerAnswerAge = sc.nextInt();
+            sc.nextLine();
+            customer1.setAge(customerAnswerAge);
 
             System.out.println("Certo! Agora digite o seu CPF. Obs.: Use apenas números.");
-            customer1.setDocument(a.nextLine());
+            customer1.setDocument(sc.nextLine());
 
 
         }else {
